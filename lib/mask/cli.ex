bbcb8448316243ddc1f8607923671b080b85d358
César González <chesarglez429@gmail.com>
2021-09-20 17:44:29 -0500

    :memo: Remove bad documentation

--- lib/mask/cli.ex
+++ lib/mask/cli.ex
@@ -1,9 +1,4 @@
 defmodule Mask.CLI do
-  @moduledoc """
-  Handle the command line parsing and the dispatch to
-  the various functions that end up generating a
-  table of the last _n_ issues in a github project
-  """
   def main(argv) do
     argv
     |> parse_args()
@@ -12,12 +7,6 @@ defmodule Mask.CLI do
     |> IO.puts()
   end
 
-  @doc """
-  `argv` can be -h or --help, which returns :help.
-  Otherwise it is a github user name, project name, and (optionally)
-  the number of entries to format.
-  Return a tuple of `{ user, project, count }`, or `:help` if help was given.
-  """
   def parse_args(argv) do
     parse =
       OptionParser.parse(argv,
