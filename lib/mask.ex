972d54e5fcd2555ddacdc497cefb361c84d177ea
César González <chesarglez429@gmail.com>
2021-09-20 18:39:06 -0500

    :speech_balloon: Update error message of Mask.of on invalid args

--- lib/mask.ex
+++ lib/mask.ex
@@ -1,9 +1,11 @@
 defmodule Mask do
-  def of(n) when n in 0..32 do
+  def of(n) when is_integer(n) and n in 0..32 do
     ones = Bits.duplicate(1, n)
     ceros = Bits.duplicate(0, 32 - n)
     <<ones::bitstring, ceros::bitstring>>
   end
+
+  def of(_), do: raise(ArgumentError, "The mask must be an integer between 0 and 32")
 
   def wildcard(n), do: Bytes.subtract(Mask.of(32), Mask.of(n))
 end
