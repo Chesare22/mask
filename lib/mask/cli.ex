4e30ae303b345de118d29c6b423298c56d3731b5
César González <chesarglez429@gmail.com>
2021-09-20 16:46:08 -0500

    :sparkles: Make mask executable by cli

---
+++ lib/mask/cli.ex
@@ -0,0 +1,44 @@
+defmodule Mask.CLI do
+  @moduledoc """
+  Handle the command line parsing and the dispatch to
+  the various functions that end up generating a
+  table of the last _n_ issues in a github project
+  """
+  def main(argv) do
+    argv |> parse_args() |> process()
+  end
+
+  @doc """
+  `argv` can be -h or --help, which returns :help.
+  Otherwise it is a github user name, project name, and (optionally)
+  the number of entries to format.
+  Return a tuple of `{ user, project, count }`, or `:help` if help was given.
+  """
+  def parse_args(argv) do
+    parse =
+      OptionParser.parse(argv,
+        switches: [wildcard: :boolean],
+        aliases: [w: :wildcard]
+      )
+
+    case parse do
+      {[wildcard: true], [number], _} ->
+        {Integer.parse(number) |> Kernel.elem(0), :wildcard}
+
+      {_, [number], _} ->
+        Integer.parse(number) |> Kernel.elem(0)
+    end
+  end
+
+  def process({number, :wildcard}) do
+    output = Mask.wildcard(number) |> Bytes4.to_string()
+    IO.puts(:stdio, output)
+    System.stop(0)
+  end
+
+  def process(number) do
+    output = Mask.of(number) |> Bytes4.to_string()
+    IO.puts(:stdio, output)
+    System.stop(0)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -1,12 +1,13 @@
-defmodule MaskCli.MixProject do
+defmodule Mask.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :mask_cli,
+      app: :mask,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      escript: escript_config(),
       deps: deps()
     ]
   end
@@ -24,5 +25,9 @@ defmodule MaskCli.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+
+  defp escript_config do
+    [main_module: Mask.CLI]
   end
 end
