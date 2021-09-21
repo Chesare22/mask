b1b88127eb347afb31edc340e8b69cf207573323
César González <chesarglez429@gmail.com>
2021-09-20 22:38:46 -0500

    :memo: Add documentation of Bytes.map

--- lib/bytes.ex
+++ lib/bytes.ex
@@ -4,6 +4,9 @@ defmodule Bytes do
   def subtract(<<minuend, minuend_tail::bitstring>>, <<subtrahend, subtrahend_tail::bitstring>>),
     do: <<minuend - subtrahend, subtract(minuend_tail, subtrahend_tail)::bitstring>>
 
+  @doc """
+  Converts a function that works on one byte into a function that works on a bitstring of multiple bytes
+  """
   def map(<<>>, _), do: <<>>
   def map(<<head, tail::bitstring>>, fun), do: <<fun.(head)::8, map(tail, fun)::bitstring>>
 
