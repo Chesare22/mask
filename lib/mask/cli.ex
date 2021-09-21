0d9b2c33c3d376ad9a09da14e2061e998053f1e6
César González <chesarglez429@gmail.com>
2021-09-21 00:24:49 -0500

    :sparkles: Add a flag to print result as binary

--- lib/mask/cli.ex
+++ lib/mask/cli.ex
@@ -2,30 +2,43 @@ defmodule Mask.CLI do
   def main(argv) do
     argv
     |> parse_args()
-    |> process()
-    |> Bytes.join(".")
+    |> process_kleisli()
+    |> result_to_string()
     |> IO.puts()
   end
 
   def parse_args(argv) do
     parse =
       OptionParser.parse(argv,
-        switches: [wildcard: :boolean],
-        aliases: [w: :wildcard]
+        switches: [wildcard: :boolean, binary: :boolean],
+        aliases: [w: :wildcard, b: :binary]
       )
 
     case parse do
-      {[wildcard: true], [number], _} ->
-        {Integer.parse(number) |> elem(0), :wildcard}
-
-      {_, [number], _} ->
-        Integer.parse(number) |> elem(0)
+      {flags, [number], _} ->
+        [
+          {:number, Integer.parse(number) |> elem(0)},
+          {:wildcard, Keyword.has_key?(flags, :wildcard)},
+          {:binary, Keyword.has_key?(flags, :binary)}
+        ]
     end
   end
 
-  def process({number, :wildcard}),
+  def process(number: number, wildcard: true),
     do: Mask.wildcard(number)
 
-  def process(number),
+  def process(number: number, wildcard: false),
     do: Mask.of(number)
+
+  def process_kleisli([number, wildcard, binary]),
+    do: [process([number, wildcard]), binary]
+
+  def result_to_string([result, binary: true]),
+    do:
+      result
+      |> Bytes.map_list(&Bits.join(&1))
+      |> Enum.join(".")
+
+  def result_to_string([result, binary: false]),
+    do: result |> Bytes.join(".")
 end
