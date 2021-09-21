29bb2d71c906c8cf9825a39ce1845d20e632b21c
César González <chesarglez429@gmail.com>
2021-09-21 00:23:37 -0500

    :sparkles: Add function Bits.join/2

--- lib/bits.ex
+++ lib/bits.ex
@@ -4,4 +4,9 @@ defmodule Bits do
 
   defp _duplicate(_, 0, acc), do: acc
   defp _duplicate(bit, n, acc), do: _duplicate(bit, n - 1, <<bit::1, acc::bitstring>>)
+
+  def join(bits, joiner \\ "")
+  def join(<<>>, _), do: ""
+  def join(<<bit::1>>, _), do: "#{bit}"
+  def join(<<bit::1, tail::bitstring>>, joiner), do: "#{bit}#{joiner}#{join(tail, joiner)}"
 end
